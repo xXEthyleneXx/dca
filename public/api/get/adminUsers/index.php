3cd0f68f31b3b8e8bbd0b15e91433ac87be7ca00
Samuel <smshelley@csustudent.net>
2023-01-28 15:59:19 -0500

    Added Organization and Comments
    Just tried to understand exactly whats going on. I think I've got the database figured out, but can't figure out what's in the array that it's returning, or how to access that array to then place it on the web page. Anyway, I'm running it on the 172...7003 and it seems to be going good
    
    Samuel Shelley
    28 January 2023

--- public/api/get/adminUsers/index.php
+++ public/api/get/adminUsers/index.php
@@ -1,15 +1,21 @@
 <?php
 
+// Require once only needs to use function once for duration
+// Replace all public with resources on server root
+// Returns api.php to be used in this file
 require_once  str_replace("public", "resources", $_SERVER["DOCUMENT_ROOT"])."/api/api.php";
 
+// Makes instance of API for tracking time
 use API;
 $API = new API\api();
 
+// Sets api as access to database
 $DBconfig = $API->config->standard["API"]["DATABASE"];
 
-$db = new mysqli($DBconfig["FIRSTNAME"], $DBconfig["LASTNAME"], $DBconfig["MIDDLEINIT"], $DBconfig["ROLE"], $DBconfig["IMAGE"]);
+// Opens DB
+$db = new mysqli($DBconfig["HOSTNAME"], $DBconfig["USERNAME"], $DBconfig["PASSWORD"], $DBconfig["DATABASE"]);
 
-$query = "select firstname, lastname, middleinit, role, image from users";
+$query = "select firstname, lastname, middleinit, role, image from adminUsers";
 $return = $db->query($query);
 $results = $return->fetch_all(MYSQLI_ASSOC);
 $db->close();
--- resources/api/resources/config.php
+++ resources/api/resources/config.php
@@ -1,19 +1,23 @@
 <?php
+
+// Configures cofig settings
 namespace Config;
 
 use Exception;
 
+// Creates config class
 class config {
 
     public array $standard;
 
-    function __construct(){
+    // config construction
+    public function __construct()
+    {
         $this->makestandard();
     }
     /**
      * Make Standard
      *  Creates a default config standard array to pull default values
-     *  
      */
     protected function makestandard() {
         $string = file_get_contents(str_replace("public", "resources", $_SERVER["DOCUMENT_ROOT"])."/config.json");
@@ -23,7 +27,6 @@ class config {
             $this->standard = json_decode($string, true);
         }
     }
-
 }
 
 ?>
